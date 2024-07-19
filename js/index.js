d54a26768c83f95201ea55dd3ffc884ad1bd77c8
linomp <linomp95@hotmail.com>
2024-07-20 00:34:37 +0200

    update script

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
-const ENABLE_AUDIO = true;
+const audio = new Audio()
+audio.autoplay = false;
+audio.loop = true;
+audio.type = 'audio/mpeg';
+audio.src = './media/the_descent_clip.mp3';
+audio.volume = 0.15;
 
-(function () {
-    if (!ENABLE_AUDIO) return;
-    var audio = new Audio()
-    audio.autoplay = false;
-    audio.loop = true;
-    audio.type = 'audio/mpeg';
-    audio.src = './media/the_descent_clip.mp3';
-    audio.volume = 0.15;
-    window.addEventListener('click', () => {
-        if (audio.paused) {
-            audio.play();
-        }
-    });
-})();
+const playClip = function () {
+    if (!audio.paused) {
+        audio.pause();
+
+        return;
+    }
+    audio.play();
+}
