bdb70fc7b849f5cb6905fce84ddd8b534e0e1787
linomp <linomp95@hotmail.com>
2024-07-20 00:16:14 +0200

    add tailwind & page structure

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const ENABLE_AUDIO = true;
+
+(function () {
+    if (!ENABLE_AUDIO) return;
+    var audio = new Audio()
+    audio.autoplay = false;
+    audio.loop = true;
+    audio.type = 'audio/mpeg';
+    audio.src = './media/the_descent_clip.mp3';
+    audio.volume = 0.15;
+    window.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+})();
