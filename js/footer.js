c0c6f7a6f3c3bc4a02e180a0b398376e8dee3932
linomp <linomp95@hotmail.com>
2024-07-20 02:37:42 +0200

    separe nav , header & footer into components & add roboto mono font

---
+++ js/footer.js
@@ -0,0 +1,23 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <footer class="w-full border-t bg-black pb-12 text-white">
+                <div class="w-full container mx-auto flex flex-col items-center">
+                    <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
+                        <a href="/index.html" class="uppercase px-3">About me</a>
+                        <a href="https://www.youtube.com/@thisaintmyrealname1/featured" class="uppercase px-3"
+                            target="_blank">videos</a>
+                        <a href="#" class="uppercase px-3">Contact</a>
+                    </div>
+                    <div class="lowercase pb-6">&copy; 2024 // p o i n t l e s s</div>
+                </div>
+            </footer>
+        `;
+    }
+}
+
+customElements.define('footer-component', Footer);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
   ],
