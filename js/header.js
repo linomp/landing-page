27d0ef16e13ae504f42f0d4e743b21f59f90823d
linomp <linomp95@hotmail.com>
2024-07-20 10:14:29 +0200

    fix web component issues

--- js/header.js
+++ js/header.js
@@ -1,21 +1,29 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+  <style>
+    @import url("../css/output.css");
+  </style>
+  <header class="w-full container mx-auto">
+    <div class="flex flex-col items-center py-12">
+      <a class="text-medium font-bold text-yellow-200 hover:text-yellow-50 text-5xl" href="#">
+        { }
+      </a>
+      <p class="text-small text-white mt-4">
+        Between two worlds
+      </p>
+    </div>
+  </header>
+`;
+
 class Header extends HTMLElement {
     constructor() {
         super();
+        this.attachShadow({ mode: 'open' });
     }
 
     connectedCallback() {
-        this.innerHTML = `
-            <header class="w-full container mx-auto">
-                <div class="flex flex-col items-center py-12">
-                    <a class="text-medium  font-bold text-yellow-200 hover:text-yellow-50 text-5xl" href="#">
-                        { }
-                    </a>
-                    <p class="text-small text-white mt-4">
-                        Between two worlds
-                    </p>
-                </div>
-            </header>
-        `;
+        this.shadowRoot.appendChild(headerTemplate.content.cloneNode(true));
     }
 }
 
